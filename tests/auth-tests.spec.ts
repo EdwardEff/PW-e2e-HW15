c06ede8259690e6a8a68527b3ce4d28310ef5f16
Edward <yurest3it@gmai;.com>
2026-01-06 22:54:49 +0200

    PR-16: Prettier

--- tests/auth-tests.spec.ts
+++ tests/auth-tests.spec.ts
@@ -7,31 +7,29 @@ if (!APP_URL) throw new Error("APP_URL is not defined");
 let faker: any;
 
 test.beforeAll(async () => {
-    faker = (await import("@faker-js/faker")).faker;
+  faker = (await import("@faker-js/faker")).faker;
 });
 
 test.beforeEach(async ({ page }) => {
-    await page.goto(APP_URL);
-    await page.waitForLoadState("networkidle");
+  await page.goto(APP_URL);
+  await page.waitForLoadState("networkidle");
 });
 
 test("Login button disabled when one field is empty", async ({ page }) => {
-    const username = faker.internet.username();
+  const username = faker.internet.username();
 
-    const login = page.getByTestId("username-input");
-    const submit = page.getByTestId("signIn-button");
+  const login = page.getByTestId("username-input");
+  const submit = page.getByTestId("signIn-button");
 
-    await expect(submit).toBeEnabled();
-    await login.fill(username);
-    await expect(submit).toBeDisabled();
+  await expect(submit).toBeEnabled();
+  await login.fill(username);
+  await expect(submit).toBeDisabled();
 });
 
 test("Auth error modal appears for invalid credentials", async ({ page }) => {
-    await page.getByTestId("username-input").fill(faker.internet.username());
-    await page.getByTestId("password-input").fill(faker.internet.password());
-    await page.getByTestId("signIn-button").click();
+  await page.getByTestId("username-input").fill(faker.internet.username());
+  await page.getByTestId("password-input").fill(faker.internet.password());
+  await page.getByTestId("signIn-button").click();
 
-    await expect(
-        page.getByTestId("authorizationError-popup")
-    ).toBeVisible();
+  await expect(page.getByTestId("authorizationError-popup")).toBeVisible();
 });
